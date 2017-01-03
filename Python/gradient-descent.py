93eef1296efc9997fc51f7698aedfe9b55521f01
Kartheek Palepu <kartheekpnsn@gmail.com>
2017-01-03 16:33:22 +0530

    Create gradient-descent.py

--- Python/gradient-descent.py
+++ Python/gradient-descent.py
@@ -39,37 +39,54 @@ def derivative2(y, yhat, x):
 def yEQ(x, theta0, theta1):
 	return [(theta0 + (i * theta1)) for i in x]
 
-def plot(oX, oY, pX, pY, m, c, text = ""):
+
+def plot(oX, oY, pX, pY, slope, y_intercept, text = ""):
 	import matplotlib.pyplot as plt
-	line = plt.figure()
-	plt.plot(oX, oY, "o")
-	plt.plot(pX, pY, 'k-', lw = 2)
-	plt.axis((0, 12, -5, 30))
+	# # plot 1st plot
+	fig, (ax1, ax2) = plt.subplots(1, 2)
+	ax1.plot(oX, oY, "o")
+	ax1.plot(pX, pY, 'k-', lw = 2)
+	ax1.set_xlim([0, 12])
+	ax1.set_ylim([-5, 30])
 	if text != "":
-		plt.annotate(text, xy=(1, 27), xytext=(1, 27))
-	plt.annotate('slope = ' + str(m) + ', c = ' + str(c), xy=(2, 1), xytext=(3, 1.5))
+		ax1.annotate(text, xy=(1, 27), xytext=(1, 27))
+	ax1.annotate('slope = ' + str(slope[-1]) + ', intercept = ' + str(y_intercept[-1]), xy=(2, 1), xytext=(3, 1.5))
+
+	# # plot 2nd plot
+	ax2.plot(slope, y_intercept, 'k-', lw = 2)
+	ax2.set_xlim([0, 10])
+	ax2.set_ylim([0, 10])
 	plt.show()
+
 
 alpha = 0.01
 threshold = 0.0001
 # let y = 3 + 2x (theta0 + theta1 * x)
 x = range(1, 11)
 y = yEQ(x, theta0 = 3, theta1 = 2) # [3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
+
+# # initialize values
 theta0 = 0
 theta1 = 0
+slope = []
+y_intercept = []
 plotFlag = True
+
+# # start iterations
 for i in range(10000):
 	print "ITERATION - " + str(i)
 	yhat = yEQ(x, theta0, theta1)
+	slope.append(theta1)
+	y_intercept.append(theta0)
 	if plotFlag:
 		if i%1000 == 0:
-			plot(x, y, x, yhat, m = theta1, c = theta0)
+			plot(x, y, x, yhat, slope, y_intercept)
 	if loss(y, yhat) <= threshold:
 		print "== ENDED at " + str(i) + " =="
 		print theta0, theta1
 		print loss(y, yhat)
 		if plotFlag:
-			plot(x, y, x, yhat, m = theta1, c = theta0, text = "Iterations ended at = " + str(i) + ", with loss = " + str(loss(y, yhat)))
+			plot(x, y, x, yhat, slope, y_intercept, text = "Iterations ended at = " + str(i) + ", with loss = " + str(loss(y, yhat)))
 		break;
 	print "For " + str(theta0) + " & " + str(theta1) + ": Loss is " + str(loss(y, yhat))
 	theta0 = round(theta0 - alpha * derivative1(y, yhat), 4)
